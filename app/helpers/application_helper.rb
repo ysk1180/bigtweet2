c2f35c274289d4baf9cb6a62f7bbd249f69055a0
Yusuke Miyazaki <ysk1180@gmail.com>
2018-09-30 14:55:17 +0900

    [change]OGPの説明文追記

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
       twitter_card[:url] = 'https://bigtweet2.herokuapp.com/'
       twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/bigtweet2/master/app/assets/images/top.png"
     end
-    twitter_card[:title] = "BigTweet"
+    twitter_card[:title] = "BigTweet〜簡単！画像に文字入りツイート（会員登録不要）"
     twitter_card[:card] = 'summary_large_image'
     twitter_card[:description] = '会員登録不要！10秒で画像に文字入りのツイートをしてみよう！'
     twitter_card
