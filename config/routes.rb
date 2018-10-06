ddf458878f1039558cb7142cb010a3d42f85ff05
Yusuke Miyazaki <ysk1180@gmail.com>
2018-10-07 08:10:29 +0900

    [change]コントローラのリファクタリング（showアクションを使用しない形に）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'terms', to: 'terms#index', as: :terms
   get 'privacy', to: 'privacies#index', as: :privacy
-  resources :posts, only: [:create, :show]
+  resources :posts, only: [:create]
   root to: "posts#new"
   post 'make', to: 'posts#make'
 end
